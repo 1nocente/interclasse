ccf56c95ee2a1a9625528f1fc09caaf6eea80c2e
Estela Alves <123739850+estelalm@users.noreply.github.com>
2023-10-28 16:55:13 -0300

    arrumando a segunda página (começo do js)

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const clicarAustralia = document.getElementById('oitoA')
+const clicarJapao = document.getElementById('oitoB')
+const clicarAfricaDoSul = document.getElementById('noveA')
+const clicarEspanha = document.getElementById('noveB')
+
+//colocar a informação do país no card quando clicar
+let mudarInfoPais = function(){ 
+}
